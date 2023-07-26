6c6457d7c8d549a66f51dfc5d7efdc889941426f
Tomas Ma?iulis <thomas.maciulis@gmail.com>
2023-07-26 12:51:41 +0300

    update naming

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import DiscountManager from './discount-manager/discount-manager'
 import User from './entity/user.aggregate-root'
 
-const serviceClient = new User()
+const user = new User()
 const discountManager = new DiscountManager()
 
-serviceClient.addDeliveryOrdersFromDataFile(process.argv[2] ?? 'input.txt')
+user.addDeliveryOrdersFromDataFile(process.argv[2] ?? 'input.txt')
 
-discountManager.applyDiscounts(serviceClient)
+discountManager.applyDiscounts(user)
 
-console.log(serviceClient.deliveryOrdersAsString)
+console.log(user.deliveryOrdersAsString)
